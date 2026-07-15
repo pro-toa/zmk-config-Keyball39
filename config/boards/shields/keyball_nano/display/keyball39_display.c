3c25e86bb47a04fd249a601981917112b67a54f0
Masahiro Okabe <masahiro0841@gmail.com>
2026-07-15 09:14:22 +0900

    Add layer-aware OLED logging

--- config/boards/shields/keyball_nano/display/keyball39_display.c
+++ config/boards/shields/keyball_nano/display/keyball39_display.c
@@ -2,10 +2,14 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
 #include <zephyr/logging/log.h>
+#include <zmk/event-manager.h>
+#include <zmk/events/layer.h>
+#include <zmk/keymap.h>
 
 LOG_MODULE_REGISTER(keyball39_display, LOG_LEVEL_INF);
 
 static const struct device *display_dev;
+static int current_layer = 0;
 
 static void draw_status_screen(void)
 {
@@ -14,6 +18,23 @@ static void draw_status_screen(void)
     }
 
     display_blanking_off(display_dev);
+
+    if (current_layer == 1) {
+        LOG_INF("MOUSE layer active");
+    } else {
+        LOG_INF("Layer: %d", current_layer);
+    }
+}
+
+static void layer_state_changed(const struct zmk_event_header *eh)
+{
+    struct zmk_layer_state_changed *event = CONTAINER_OF(eh, struct zmk_layer_state_changed, header);
+    current_layer = event->layer;
+    draw_status_screen();
+}
+
+ZMK_EVENT_CONSUMER(layer_state_changed) {
+    return zmk_layer_state_changed_from_state(event);
 }
 
 static int keyball39_display_init(void)
